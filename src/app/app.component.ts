986a2c28d48aefa2e3f1f1a88a951c82bdd298cb
PepoCivs <tcbpepo@gmail.com>
2018-09-25 13:42:03 +0200

    style: move styles to each component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 
 import resumeData from './data/my-resume';
-import { ProfileHeaderComponent } from './components/profile-header/profile-header.component';
-import { StructureComponent } from './components/structure/structure.component';
-import { TimelineComponent } from './components/timeline/timeline.component';
 
 @Component({
   selector: 'app-root',

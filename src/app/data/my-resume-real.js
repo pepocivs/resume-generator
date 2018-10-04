832f5950ca4d5a986bc72c8d578e9fbf217c63c7
PepoCivs <tcbpepo@gmail.com>
2018-10-04 02:23:32 +0200

    chore: accurating style

--- src/app/data/my-resume-real.js
+++ src/app/data/my-resume-real.js
@@ -7,6 +7,7 @@ export default {
     "I'm a startupGuy, looking for cool projects based on last technologies and always trying to learn, I like to develop my own projects along the way.",
     "<strong>\"I believe that something can always be better.\"</strong>"
   ],
+  "legend": "* 100% of the skills bars represents my knowledge in relation with my other knowledge, it doesn't represent 100% of the worldwide knowledge :) [I didn't not create JavaScript]",
   "background": [
     {
       "name": "Github",
@@ -27,7 +28,6 @@ export default {
   "contact": {
     "phone": "+34 655 358 208",
     "email": "tcbpepo@gmail.com",
-    "skype": "locoporlapaz",
     "socialNetworks": "@PepoCivs",
     "address": "C/. Laguna del Marquesado, 11"
   },
@@ -38,6 +38,7 @@ export default {
         "title": "Information technology degree",
         "location": "Universidad Politécnica (Valencia)",
         "highLight": "High academic performance group",
+        "highLightIcon": "work",
         "duration": "Until July 2012 (2nd course)",
         "icon": "academic"
       },
@@ -46,6 +47,7 @@ export default {
         "title": "Apps development (DAI)",
         "location": "I.E.S. Abastos (Valencia)",
         "highLight": "New House Designs (Plymouth, UK)",
+        "highLightIcon": "work",
         "duration": "Until July 2010",
         "icon": "academic"
       },
@@ -54,17 +56,28 @@ export default {
         "title": "Middle degree",
         "location": "Tame (Mislata)",
         "highLight": "Ford Almussafes (Valencia)",
+        "highLightIcon": "work",
         "duration": "Until June 2008",
         "icon": "academic"
       }
     ],
     "experience": [
       {
-        "year": "2017",
+        "year": "2018",
         "title": "Senior Developer",
+        "location": "SopraSteria (Madrid)",
+        "highLight": "Polymer & Lion Components",
+        "highLightIcon": "code",
+        "duration": "Current",
+        "icon": "work"
+      },
+      {
+        "year": "2017",
+        "title": "Product Hacker",
         "location": "Product Hackers (Madrid)",
         "highLight": "JavaScript (ES6), NodeJs & AWS",
-        "duration": "Current",
+        "highLightIcon": "code",
+        "duration": "Until December 2017",
         "icon": "work"
       },
       {
@@ -72,7 +85,71 @@ export default {
         "title": "Front Leader",
         "location": "King of App (Madrid)",
         "highLight": "Polymer, Angular, NodeJs, MongoDB & AWS",
+        "highLightIcon": "code",
         "duration": "Until July 2017",
+        "icon": "work"
+      },
+      {
+        "year": "2014",
+        "title": "Junior Fullstack Developer",
+        "location": "TriboTV (Madrid)",
+        "highLight": "PHP, MySql, CSS, JavaScript & Java",
+        "highLightIcon": "code",
+        "duration": "Until November 2014",
+        "icon": "work"
+      },
+      {
+        "year": "2014",
+        "title": "Selected for 1st SM edition",
+        "location": "Startups Mansion (New York, US)",
+        "highLight": "30 Entepreneurs living 3 months in NYC",
+        "highLightIcon": "info",
+        "duration": "Until December 2014",
+        "icon": "award"
+      },
+      {
+        "year": "2013",
+        "title": "Junior Fullstack Developer",
+        "location": "Homelyst (Valencia)",
+        "highLight": "PHP, MySql, CSS & JavaScript",
+        "highLightIcon": "code",
+        "duration": "Until January 2015",
+        "icon": "work"
+      },
+      {
+        "year": "2013",
+        "title": "Co-founder & Developer",
+        "location": "Interacis (Valencia)",
+        "highLight": "PHP, MySql, CSS & JavaScript",
+        "highLightIcon": "code",
+        "duration": "Until January 2017",
+        "icon": "work"
+      },
+      {
+        "year": "2012",
+        "title": "Co-founder & Developer",
+        "location": "Sopcial S.L. (Valencia)",
+        "highLight": "PHP, MySql, CSS & Jquery",
+        "highLightIcon": "code",
+        "duration": "Until September 2013",
+        "icon": "work"
+      },
+      {
+        "year": "2012",
+        "title": "Accelerator Program Winners",
+        "location": "Business Booster",
+        "highLight": "Selected with our startup \"Sopcial\"",
+        "highLightIcon": "code",
+        "duration": "Until february 2013",
+        "icon": "award"
+      },
+      {
+        "year": "2010",
+        "title": "Founder & Developer",
+        "location": "Tuintra (Valencia)",
+        "highLight": "PHP, MySql, CSS & Jquery",
+        "highLightIcon": "code",
+        "duration": "Current",
         "icon": "work"
       }
     ]
@@ -119,6 +196,18 @@ export default {
       {
         "name": "TDD - Protractor / Jasmine",
         "percent": "60"
+      },
+      {
+        "name": "Git",
+        "percent": "90"
+      },
+      {
+        "name": "C.I. (jenkins)",
+        "percent": "70"
+      },
+      {
+        "name": "AWS",
+        "percent": "60"
       }
     ],
     "framework": [
@@ -138,14 +227,14 @@ export default {
         "name": "Vue (JavaScript)",
         "percent": "60"
       },
-      {
-        "name": "React (JavaScript)",
-        "percent": "60"
-      },
-      {
-        "name": "Laravel (PHP)",
-        "percent": "60"
-      }
+      // {
+      //   "name": "React (JavaScript)",
+      //   "percent": "60"
+      // },
+      // {
+      //   "name": "Laravel (PHP)",
+      //   "percent": "60"
+      // }
     ],
     "misc": [
       {
@@ -165,9 +254,46 @@ export default {
         "percent": "90"
       },
       {
-        "name": "Geographical mobility (JavaScript)",
+        "name": "Geographical mobility",
         "percent": "100"
       }
     ]
-  }
+  },
+  "microProjects": [
+    {
+      "year": "2017",
+      "name": "Recipes of the wild",
+      "type": "Progressive web App",
+      "mainFront": "Polymer",
+      "mainBack": "NodeJS"
+    },
+    {
+      "year": "2017",
+      "name": "DGT test",
+      "type": "Progressive web App",
+      "mainFront": "Polymer",
+      "mainBack": "NodeJS"
+    },
+    {
+      "year": "2016",
+      "name": "Chat module for KOA",
+      "type": "App Module",
+      "mainFront": "AngularJS",
+      "mainBack": "Firebase"
+    },
+    {
+      "year": "2016",
+      "name": "renfe price comparator Module",
+      "type": "App Module",
+      "mainFront": "Polymer",
+      "mainBack": "PHP"
+    },
+    {
+      "year": "2011",
+      "name": "Popubooks",
+      "type": "Social network",
+      "mainFront": "PHP",
+      "mainBack": "PHP"
+    }
+  ]
 }

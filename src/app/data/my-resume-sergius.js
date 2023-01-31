415b618354ac5d9deadb1c35a21d1b357b31e343
Jorge Martin <tcbpepo@gmail.com>
2023-01-31 13:39:54 +0100

    chore: new data for sergius

--- src/app/data/my-resume-sergius.js
+++ src/app/data/my-resume-sergius.js
@@ -1,9 +1,10 @@
 export default {
   "template": 'arts',
+  "type": 'artistics',
   "fullName": "Sergio Olmeda",
   "picture": "https://es.gravatar.com/userimage/198969001/5cb06dfec1883cc56794d2cd537f4d24.jpg?size=600",
   "aboutMe": [
-    "Me importa mucho el cuidado de la piel. Es por esto que, de manera autodidacta, me gusta invertir mi tiempo en leer y consultar información sobre los distintos principios activos y problemáticas de la piel. Considero que mi conocimiento se podría aplicar a una buena orientación de cara al cliente."
+    "Debido a mi formación, considero muy importante el valor del diseño y la creatividad. Por tanto, valoro trabajar en una empresa que se dedica al diseño aplicado al mercado textil y que ostenta una posición internacional en moda y tendencias."
   ],
   "profile": [
     "Actualmente, me encuentro en búsqueda activa de empleo y me interesa adquirir experiencia en el mundo laboral.  En cuanto a mis aptitudes, soy organizado y metódico, considero muy importante la buena comunicación y el trabajo en equipo así como la productividad y el desarrollo personal."
@@ -24,6 +25,10 @@ export default {
       },
       {
         "name": "Indesign",
+        "value": "Alto"
+      },
+      {
+        "name": "Premiere",
         "value": "Alto"
       },
       {
@@ -49,7 +54,7 @@ export default {
       },
       {
         "icon": "address",
-        "value": "C/. Isla Cabrera, 8"
+        "value": "Avenida Barcelona 92, 57"
       },
       {
         "icon": "website",
@@ -59,14 +64,91 @@ export default {
   },
   "education": [
     {
-      "year": "2017 - 2021",
-      "title": "Graduado en Bellas Artes",
-      "subTitle": [ "Universitat Politècnica de València" ]
-    },
-    {
       "year": "2021 - 2022",
       "title": "Máster en producción artística",
       "subTitle": [ "Universitat Politècnica de València" ]
+    },
+    {
+      "year": "2017 - 2021",
+      "title": "Graduado en Bellas Artes",
+      "subTitle": [ "Universitat Politècnica de València" ]
+    }
+  ],
+  "certificates": [
+    {
+      "year": "2016",
+      "title": "Certificado de manipulador de alimentos",
+      "subTitle": [ "IES Albal" ]
+    }
+  ],
+  "courses": [
+    {
+      "year": "28.10.2020 - 30.10.2020",
+      "title": "(EN) PROCESO. Laboratori amb Inma Femenía",
+      "subTitle": [ "Bombas Gens Centre d'Art" ]
+    },
+  ],
+  "projects": [
+    {
+      "year": "16.12.2021 – 06.02.2022",
+      "title": "Colectiva 1.0",
+      "subTitle": [ "Exposición colectiva en Abadía Seven. Villafranca del Cid, Castellón." ]
+    },
+    {
+      "year": "11.06.2021 – 26.06.2021",
+      "title": "La sexta lámpara. La fragilidad de la memoria",
+      "subTitle": [ "Exposición colectiva en Abadía Seven. Villafranca del Cid, Castellón." ]
+    },
+    {
+      "year": "24.05.2021 – 23.06.2021",
+      "title": "Self-light. Luz y medio digital",
+      "subTitle": [ "Exposición individual en la sala de exposiciones del Casal Jove d'Albal." ]
+    },
+    {
+      "year": "21.05.2021 – 23.07.2021",
+      "title": "Proyectar[se]",
+      "subTitle": [ "Exposición colectiva en el Colegio Oficial de Arquitectos de Castilla-La Mancha (COACMTO). Toledo, Castilla-La Mancha." ]
+    },
+    {
+      "year": "05.05.2021 – 22.05.2021",
+      "title": "Infinity Art",
+      "subTitle": [ "Exposición colectiva en la sala de exposiciones del Corte Inglés de calle Colón, València." ]
+    }
+  ],
+  "publications": [
+    {
+      "year": "2021",
+      "title": "'Self-light. Luz y medio digital', una exposició de Sergio Olmeda que emplenarà el Casal Jove d'Albal",
+      "subTitle": [
+        "<a href=\"https://www.ivoox.com/self-light-luz-medio-digital-exposicio-de-audios-mp3_rf_70438653_1.html\">https://www.ivoox.com/self-light-luz-medio-digital-exposicio-de-audios-mp3_rf_70438653_1.html</a>",
+        "Entrevista en el programa El Rall de Helena Montaner en Ràdio À Punt."
+      ],
+      "size": "medium",
+    },
+    {
+      "year": "2021",
+      "title": "SELF-LIGHT. LUZ Y MEDIO DIGITAL EN EL CASAL JOVE DE ALBAL",
+      "subTitle": [
+        "<a href=\"https://www.visualartcv.com/self-light-luz-y-medio-digital-de-sergio-olmeda-en-el-casal-jove-de-albal\">https://www.visualartcv.com/self-light-luz-y-medio-digital-de-sergio-olmeda-en-el-casal-jove-de-albal</a>",
+        "Artículo sobre la exposición individual Self-light. Luz y medio digital."
+      ],
+      "size": "medium",
+    },
+    {
+      "year": "2021",
+      "title": "Bajo el prisma de la identidad",
+      "subTitle": [
+        "<a href=\"https://www.latribunadetalavera.es/Noticia/Z7186EE71-F3A9-84F9-C957840C521ADDB8/202105/Bajo-elprisma-de-la-identidad\">https://www.latribunadetalavera.es/Noticia/Z7186EE71-F3A9-84F9-C957840C521ADDB8/202105/Bajo-elprisma-de-la-identidad</a>",
+        "Artículo sobre la exposición colectiva Proyectar[se] en COACMTO, Toledo."
+      ]
+    },
+    {
+      "year": "2021",
+      "title": "EXPOSICIÓN INFINITY ART \"ARTE VIVO INTERACTIVO\"",
+      "subTitle": [
+        "<a href=\"https://www.centroscomerciales.elcorteingles.es/centroscomerciales/es/eci/centros/centro-comercialpintor-sorolla-colon/eventos/exposicion-infinity-art-arte-vivo-interactivo\">https://www.centroscomerciales.elcorteingles.es/centroscomerciales/es/eci/centros/centro-comercialpintor-sorolla-colon/eventos/exposicion-infinity-art-arte-vivo-interactivo</a>",
+        "Breve artículo sobre la exposición colectiva Infinity Art."
+      ]
     }
   ],
   "languages": [
@@ -91,6 +173,22 @@ export default {
       "title": "Francés",
       "subTitle": [
         "A2",
+        "Centro de Secundaria.<br />I.E.S. Albal"
+      ]
+    }
+  ],
+  "realLanguages": [
+    {
+      "year": "2021",
+      "title": "Inglés - B2",
+      "subTitle": [
+        "Examen Certacles.<br />Centro de Lenguas de la Universitat Politècnica de València"
+      ]
+    },
+    {
+      "year": "2016",
+      "title": "Francés - A2",
+      "subTitle": [
         "Centro de Secundaria.<br />I.E.S. Albal"
       ]
     }
---
+++ src/app/data/my-resume-stefania.js
@@ -0,0 +1,222 @@
+export default {
+  "fullName": "Neila E. Torres Panez",
+  "title": "Software Engineer | Frontend Developer",
+  "picture": "https://secure.gravatar.com/avatar/a374670c7665ca0202a892010dee4e43?size=1024",
+  "aboutMe": [
+    "Enthusiastic, diligent and detail-oriented Software Engineer eager to contribute to team success.",
+    "Strong commitment to collaboration and solutions-oriented problem-solving. Implements with various web design frameworks to develop custom-crafted, customer-focused websites and designs.Dedicated to writing superior quality code and building a great software base on maximized user experience, usability and performance.",
+    "<strong>\"you learn what you teach and own what you share.\"</strong>"
+  ],
+  "legend": "* 100% of the skills bars represents my knowledge in relation with my other knowledge, it doesn't represent 100% of the worldwide knowledge :) [I didn't not create JavaScript]",
+  "background": [
+    {
+      "name": "Github",
+      "icon": "github",
+      "url": "github.com/netorrespanez"
+    },
+    {
+      "name": "Linkedin",
+      "icon": "linkedin",
+      "url": "linkedin.com/in/etorrespanez"
+    }
+  ],
+  "contact": {
+    "phone": "+34 603 151 135",
+    "email": "ne.torrespanez@gmail.com",
+    "socialNetworks": "@netorrespanez",
+    "address": "C/. Lorenzo Gonzalez, Madrid"
+  },
+  "timeline": {
+    "education": [
+      {
+        "year": "2021",
+        "title": "European Master in Software Engineering",
+        "location": "Universidad Politécnica de Madrid (Madrid)",
+        "highLight": "High academic performance group",
+        "highLightIcon": "work",
+        "duration": "Until July 2023",
+        "icon": "academic"
+      },
+      {
+        "year": "2012",
+        "title": "Bachelor's Degree in Computer Science",
+        "location": "Universidad Politécnica de Madrid (Madrid)",
+        "highLight": "High academic performance group",
+        "highLightIcon": "work",
+        "duration": "Until July 2017",
+        "icon": "academic"
+      }
+    ],
+    "experience": [
+      {
+        "year": "2022",
+        "title": "Senior Software Engineer - Front End Dev",
+        "location": "nc43tech - NextChance Invest (Madrid)",
+        "highLight": "Nuxt.js · JavaScript · Vue.js",
+        "highLightIcon": "code",
+        "duration": "Current",
+        "icon": "work"
+      },
+      {
+        "year": "2021",
+        "title": "Software Engineer - Accessibility Advocate",
+        "location": "Capgemini Engineering (Madrid)",
+        "highLight": "Develop accessible, user-oriented, cross-platform applications. Develop accessibility reviews of applications. Make talks and training on web accessibility",
+        "duration": "Until March 2022",
+        "icon": "work"
+      },
+      {
+        "year": "2019",
+        "title": "Web Development Teacher",
+        "location": "Fictizia,(Madrid)",
+        "highLight": "JavaScript and FrontEnd Frameworks (Vue)",
+        "highLightIcon": "code",
+        "duration": "Until July 2021",
+        "icon": "work"
+      },
+      {
+        "year": "2018",
+        "title": "Senior Frontend Developer",
+        "location": "SopraSteria [BBVA-GloMo] (Madrid)",
+        "highLight": "JavaScript (ES6), Polymer (Cells)",
+        "highLightIcon": "code",
+        "duration": "Until December 2019",
+        "icon": "work"
+      },
+      {
+        "year": "2017",
+        "title": "Data Analyst",
+        "location": "Iberia Airlines (Madrid)",
+        "highLight": "R Studio",
+        "highLightIcon": "code",
+        "duration": "Until July 2018",
+        "icon": "work"
+      },
+      {
+        "year": "2016",
+        "title": "Chatbot Developer",
+        "location": "Commons.fm (Madrid)",
+        "highLight": "JavaScript (ES6), NodeJs",
+        "highLightIcon": "code",
+        "duration": "Until July 2017",
+        "icon": "work"
+      }
+    ]
+  },
+  "languages": [
+    {
+      "name": "Spanish",
+      "level": "Native",
+      "percent": "100"
+    },
+    {
+      "name": "English",
+      "level": "B2",
+      "percent": "70"
+    },
+    {
+      "name": "French",
+      "level": "B2",
+      "percent": "70"
+    }
+  ],
+  "skills": {
+    "technical": [
+      {
+        "name": "JavaScript",
+        "percent": "90"
+      },
+      {
+        "name": "MySql",
+        "percent": "80"
+      },
+      {
+        "name": "CSS - SASS / LESS",
+        "percent": "70"
+      },
+      {
+        "name": "NoSql (MongoDB)",
+        "percent": "90"
+      },
+      {
+        "name": "TDD - Chai / Mocha",
+        "percent": "60"
+      },
+      {
+        "name": "Git",
+        "percent": "90"
+      },
+      {
+        "name": "C.I. (Jenkins)",
+        "percent": "60"
+      },
+      {
+        "name": "R Studio",
+        "percent": "60"
+      }
+    ],
+    "framework": [
+      {
+        "name": "Polymer (JavaScript)",
+        "percent": "90"
+      },
+      {
+        "name": "React (JavaScript)",
+        "percent": "60"
+      },
+      {
+        "name": "Angular 1.5 (JavaScript)",
+        "percent": "60"
+      },
+      {
+        "name": "NodeJS (JavaScript)",
+        "percent": "70"
+      },
+      {
+        "name": "Vue (JavaScript)",
+        "percent": "90"
+      },
+      {
+        "name": "JAVA, J2EE (JMS, Java RMI)",
+        "percent": "60"
+      }
+    ],
+    "misc": [
+      {
+        "name": "Agile (Scrum)",
+        "percent": "70"
+      },
+      {
+        "name": "Learning by doing",
+        "percent": "80"
+      },
+      {
+        "name": "TeamWork",
+        "percent": "90"
+      },
+      {
+        "name": "Startup development",
+        "percent": "90"
+      },
+      {
+        "name": "Code Reviews",
+        "percent": "80"
+      }
+    ]
+  },
+  "microProjects": [
+    {
+      "year": "2021",
+      "name": "Web Scraping Stackbuilders",
+      "type": "Script",
+      "mainFront": "Puppeteer and Cheerio",
+      "mainBack": "NodeJS"
+    },
+    {
+      "year": "2020",
+      "name": "CFF Map",
+      "type": "Single-Page App",
+      "mainFront": "Vue.js"
+    }
+  ]
+}

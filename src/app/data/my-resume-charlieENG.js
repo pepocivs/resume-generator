e2be06c9800eb5f8183a5440e9d34ce6ae1ed9da
Francisco Carlos Beltran Arsene <78327050+cuantocarlos@users.noreply.github.com>
2024-03-06 03:50:17 +0100

    Create my-resume-charlieENG.js

---
+++ src/app/data/my-resume-charlieENG.js
@@ -0,0 +1,250 @@
+export default {
+  "template": "two-columns",
+  "fullName": "Carlos Beltran",
+  "title": "IT Developer",
+  "picture": "https://gravatar.com/userimage/171510680/28a59c39f278f7b65eb85fe330adf893.jpeg?size=600",
+  "aboutMe": [
+    "My name is Fco.Carlos and my passion for technology and computers defines me. I started my career as a technician in Microcomputer Systems and Networks, where I gained experience in computer support, repair and assembly of computers.",
+    "During an enriching period of 4 months, I had the opportunity to do an internship in the UK, where I polished my technical and linguistic skills in English. After my internship, I worked at an Apple Premium Reseller in Ibiza, where I gained experience in repairing and assembling Apple computers, customer service and product sales.",
+    "I have recently completed my Bachelor Degree in Web Application Development, where I have acquired skills in technologies such as responsive Bootstrap, JavaScript, AJAX, Java, PHP with Model View Controller and database management, as well as familiarizing myself with Frameworks such as Laravel or Vue and AWS. I am also open to training in other technologies such as SWIFT, , Python, Go, among others.",
+    "Geographical mobility is not an obstacle for me. I am a versatile and creative person, always looking for innovative solutions to any challenge. I am eager to keep learning and growing in a professional environment."
+  ],
+  "legend": "* 100% of the skill bars represent my knowledge in relation to my other knowledge, this does not represent 100% of the world's knowledge ;) [I didn't invent Linux].",
+  "background": [],
+  "contact": {
+    "type": 'two-column-icon',
+    "data": [
+      {
+        "name": "Phone",
+        "icon": "phone",
+        "value": "+34 665 306 070"
+      },
+      {
+        "name": "E-Mail",
+        "icon": "email",
+        "value": "fco.carlos@proton.me"
+      },
+      {
+        "name": "Address",
+        "icon": "address",
+        "value": "Carolina Alvarez, 27"
+      },
+      {
+        "name": "Nationality",
+        "icon": "nationality",
+        "value": "Spanish"
+      },
+      {
+        "name": "Date of Birth",
+        "icon": "birthday",
+        "value": "04/08/1998"
+      }
+    ]
+  },
+  "education": [
+    {
+      "year": "Act",
+      "title": "Higher Technician in Web Applications Development",
+      "location": "I.E.S. Abastos (Valencia)",
+      "highLight": "-",
+      "highLightIcon": "code",
+      "icon": "academic"
+    },
+    {
+      "year": "2021",
+      "title": "Microcomputer Systems and Network Technician",
+      "location": "I.E.S. Abastos (Valencia)",
+      "highLight": "Nivel 3 EQF-MEC",
+      "highLightIcon": "code",
+      "icon": "academic"
+    },
+  ],
+  "experience": [
+    {
+      "year": "2022",
+      "title": "Seller at Apple Premium Reseller",
+      "location": "Intecat Istore S.L. (Ibiza, España)",
+      "tasks": [
+        "In this position, I had the opportunity to apply my English skills that I acquired during my internship in the UK. I was also able to make the most of my experience using Apple equipment while dealing with a wide variety of problems presented by users.",
+        "In addition, I gained knowledge of the most modern invoicing systems used in the shop."
+      ],
+      "duration": "Junio 2022 - Octubre 2022",
+      "icon": "work"
+    },
+    {
+      "year": "2021",
+      "title": "IT Technician",
+      "location": "Computers for charities (Eastbourne, Reino unido)",
+      "tasks": [
+        "In 2019 I was selected from my high school for a work placement abroad (FCT) and was lucky enough to opt for the UK, where I worked for Recycling for Charities and Computers for Charities. Although my mobility was delayed until 2021 due to the COVID-19 pandemic, the experience was worth it.",
+        "During my stay in the UK, I not only improved my English, but also gained professional, work and personal experience. I learned about the assembly, maintenance, disassembly, repair and recycling of computers, laptops and smartphones, as well as customer service and warehouse logistics. I also learned about installing Windows and Linux operating systems directly from the company network, optimising the software and acquiring in-depth knowledge of the operating systems.",
+        "In addition, I learned about confidentiality, privacy and the philosophy of our clients. Although the experience had some difficult moments, such as arriving with a suitcase to a flat to live with strangers and coming to work on the second day without knowing anyone and learning to deal with challenging circumstances, it helped me grow as a person and as a worker. My boss and colleagues showed me a profound human quality, which helped me to increase my social skills, be more proactive and work as part of a team. I also learned a lot about the English culture, which is very different from the Spanish one.",
+        "All in all, this was a very enriching experience that allowed me to acquire valuable skills and grow both personally and professionally.",
+      ],
+      "duration": "May 2021 - August 2021",
+      "icon": "work"
+    },
+    {
+      "year": "-",
+      "title": "Food store",
+      "location": "Delicatessen Valencianas (Valencia, España)",
+      "tasks": [
+        "All my life I have been involved in the family business, with several positions in the Mercat Central de València. Through this experience, I have acquired skills in customer service (providing excellent customer service), food handling and knowledge of the food sector. I have worked in retail and wholesale sales, and I have learnt how to handle cash management and other administrative tasks. This experience has been fundamental to my personal and professional development."
+      ],
+      "duration": "-",
+      "icon": "work"
+    },
+    {
+      "year": "-",
+      "title": "Computer assistant",
+      "location": "Eurostars (Valencia, España)",
+      "tasks": [
+        "Consultancy.",
+        "Computer maintenance."
+      ],
+      "duration": "-",
+      "icon": "work"
+    },
+    {
+      "year": "-",
+      "title": "Carer of persons with disabilities",
+      "location": "Valencia",
+      "tasks": [],
+      "duration": "-",
+      "icon": "work"
+    },
+    {
+      "year": "-",
+      "title": "Child Care",
+      "location": "Valencia",
+      "tasks": [
+        "Care of a girl and several children.",
+        "Help with primary school homework.",
+        "Take them to school and/or pick them up.",
+      ],
+      "duration": "-",
+      "icon": "work"
+    },
+  ],
+  "languages": [
+    {
+      "name": "Spanish",
+      "level": "Native language",
+      "percent": "100"
+    },
+    {
+      "name": "Valencian",
+      "level": "Native language",
+      "percent": "100"
+    },
+    {
+      "name": "English",
+      "level": "B2/C1",
+      "percent": "80"
+    },
+    {
+      "name": "Romanian Sign Language",
+      "level": "Native language",
+      "percent": "70"
+    },
+    {
+      "name": "Romanian",
+      "level": "B1",
+      "percent": "50"
+    },
+  ],
+  "skills": {
+    "technical": {
+      "data": [
+        {
+          "name": "Java ",
+          "percent": "70"
+        },
+        {
+          "name": "PHP",
+          "percent": "88"
+        },
+        {
+          "name": "Bootstrap",
+          "percent": "60"
+        },
+        {
+          "name": "Python",
+          "percent": "30"
+        },
+        {
+          "name": "JavaScript",
+          "percent": "95"
+        },
+        {
+          "name": "S.O. Linux and Mac",
+          "percent": "85"
+        },
+        {
+          "name": "Gitflow",
+          "percent": "80"
+        },
+        {
+          "name": "Amazon Web Services",
+          "percent": "70"
+        },
+        {
+          "name": "Gitflow",
+          "percent": "100"
+        },
+      ]
+    },
+    "framework": [],
+    "misc": [
+      {
+        "name": "Teamwork",
+        "percent": "100"
+      },
+      {
+        "name": "Communication skills",
+        "percent": "100"
+      },
+      {
+        "name": "Adaptation to work",
+        "percent": "80"
+      },
+      {
+        "name": "People skills",
+        "percent": "100"
+      },
+      {
+        "name": "Thoroughness and cleanliness",
+        "percent": "95"
+      },
+      {
+        "name": "Troubleshooting",
+        "percent": "100"
+      },
+      {
+        "name": "Confidential treatment of information",
+        "percent": "100"
+      },
+    ]
+  },
+  "competences": [
+    "Communication skills.",
+    "Flexibility (I am able to adapt to the circumstances of the job).",
+    "Teamwork (experience gained in various working groups)",
+    "Accustomed to good customer service.",
+    "Knowledge for the improvement of the working environment, efficiency and safety of the space.",
+    "Ability to lead and motivate other team members.",
+  ],
+  "microProjects": [],
+  "profile": [
+    "-"
+  ],
+  "personal": {
+    "data": [
+      "Francisco Carlos Beltran Arsene",
+      "04 - 08 - 1998",
+      "Valencia"
+    ]
+  },
+  "certificates": [],
+  "software": {},
+}
